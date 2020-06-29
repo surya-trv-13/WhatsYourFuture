1b254a091b5b6827f2628d5a21a3cc3a209b1278
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2020-06-30 00:01:07 +0530

    Added async Await in the Project

--- server/server.js
+++ server/server.js
@@ -14,26 +14,26 @@ var app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/' , (request , response) => {
+app.get('/' , async (request , response) => {
     var zodiac = request.query.zodiac[0].toUpperCase() + request.query.zodiac.slice(1)
     
-    getHoroscope(zodiac).then((res) => {
+    try{
+        const res = await getHoroscope(zodiac)
         response.status(200).send({result : res.data[zodiac]});
-    }).catch((error) => {
+    }catch(error){
         response.status(500).send({error})
-    }) 
+    } 
 })
 
 // POST /user
-app.post('/user' , (request , response) => {
-    var body = _.pick(request.body, ['email' , 'password'])
-
-    var user = new UserModel(body)
-    user.save().then((res) => {
+app.post('/user' , async (request , response) => {
+    var user = new UserModel(_.pick(request.body, ['email' , 'password']))
+    try{
+        const res = await user.save()
         response.status(200).send({res})
-    }).catch((error) => {
+    }catch(error){
         response.status(400).send({error})
-    })
+    }
 })
 app.listen(PORT , () => {
     console.log(`Listening to PORT => ${PORT}`);
